fb13939ad906c03f8d2569c8979fc2fb294417dc
Filipe Siqueira <filipedev040990@gmail.com>
2023-06-19 18:27:08 -0300

    feat: ensure SavePayerUseCase call repository.save once and with correct values

--- src/application/usecases/save-payer.usecase.spec.ts
+++ src/application/usecases/save-payer.usecase.spec.ts
@@ -1,16 +1,21 @@
 import { UUIDGeneratorInterface } from '../contratcs/uuid-generator.interface'
 import { SavePayerUseCase } from './save-payer.usecase'
 import { SavePayerUseCaseInterface } from '../contratcs/save-payer-usecase.interface'
+import { SavePayerRepositoryInterface } from '../contratcs/payer-repository.interface'
 import { mock } from 'jest-mock-extended'
+import MockDate from 'mockdate'
 
 const uuidGenerator = mock<UUIDGeneratorInterface>()
+const repository = mock<SavePayerRepositoryInterface>()
 
 describe('SavePayerUseCase', () => {
   let sut: SavePayerUseCase
   let input: SavePayerUseCaseInterface.Input
 
   beforeAll(() => {
-    sut = new SavePayerUseCase(uuidGenerator)
+    MockDate.set(new Date())
+
+    sut = new SavePayerUseCase(uuidGenerator, repository)
 
     input = {
       personType: 'anyPersonType',
@@ -25,10 +30,38 @@ describe('SavePayerUseCase', () => {
       city: 'AnyCity',
       state: 'AnyState'
     }
+
+    uuidGenerator.generate.mockReturnValue('anyUUID')
   })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
   test('should call UUIDGenerator once', async () => {
     await sut.execute(input)
 
     expect(uuidGenerator.generate).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call repository.save once and with correct values', async () => {
+    await sut.execute(input)
+
+    expect(repository.save).toHaveBeenCalledTimes(1)
+    expect(repository.save).toHaveBeenCalledWith({
+      id: 'anyUUID',
+      personType: 'anyPersonType',
+      name: 'AnyName',
+      email: 'anyEmail@email.com',
+      document: 'anyDocument',
+      phoneNumber: 'anyPhoneNumber',
+      cep: 'anyCep',
+      street: 'AnyStreet',
+      number: 'anyNumber',
+      neighborhood: 'AnyNeighborhood',
+      city: 'AnyCity',
+      state: 'AnyState',
+      createdAt: new Date()
+    })
   })
 })
--- src/application/usecases/save-payer.usecase.ts
+++ src/application/usecases/save-payer.usecase.ts
@@ -1,12 +1,29 @@
+import { SavePayerRepositoryInterface } from '../contratcs/payer-repository.interface'
 import { SavePayerUseCaseInterface } from '../contratcs/save-payer-usecase.interface'
 import { UUIDGeneratorInterface } from '../contratcs/uuid-generator.interface'
 
 export class SavePayerUseCase implements SavePayerUseCaseInterface {
   constructor (
-    private readonly uuidGenerator: UUIDGeneratorInterface
+    private readonly uuidGenerator: UUIDGeneratorInterface,
+    private readonly repository: SavePayerRepositoryInterface
   ) {}
 
   async execute (input: SavePayerUseCaseInterface.Input): Promise<void> {
-    this.uuidGenerator.generate()
+    await this.repository.save({
+      id: this.uuidGenerator.generate(),
+      name: input.name,
+      email: input.email,
+      document: input.document,
+      personType: input.personType,
+      phoneNumber: input.phoneNumber,
+      cep: input.cep,
+      street: input.street,
+      number: input.number,
+      neighborhood: input.neighborhood,
+      complement: input.complement ?? undefined,
+      city: input.city,
+      state: input.state,
+      createdAt: new Date()
+    })
   }
 }
