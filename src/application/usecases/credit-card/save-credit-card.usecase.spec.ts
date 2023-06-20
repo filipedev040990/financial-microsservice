d8d06b6d52797d3f1d9e53ca76a9dcaa442b01d7
Filipe Siqueira <filipedev040990@gmail.com>
2023-06-20 12:04:38 -0300

    feat: ensure SaveCreditCardUseCase call CreditCardRepository.save once and with correct values

--- src/application/usecases/credit-card/save-credit-card.usecase.spec.ts
+++ src/application/usecases/credit-card/save-credit-card.usecase.spec.ts
@@ -1,16 +1,22 @@
 import { SaveCreditCardUseCase } from './save-credit-card.usecase'
 import { UUIDGeneratorInterface } from '@/application/contratcs/uuid-generator.interface'
 import { SaveCreditCardUseCaseInterface } from '@/application/contratcs/save-credit-card-usecase.interface'
+import { SaveCreditCardRepositoryInterface } from '@/application/contratcs/credit-card-repository.interface'
 import { mock } from 'jest-mock-extended'
+import MockDate from 'mockdate'
 
 const uuidGenerator = mock<UUIDGeneratorInterface>()
+const repository = mock<SaveCreditCardRepositoryInterface>()
+const identifierGeneratorMock = jest.spyOn(SaveCreditCardUseCase.prototype as any, 'identifierGenerator')
 
 describe('SaveCreditCardUseCase', () => {
   let sut: SaveCreditCardUseCase
   let input: SaveCreditCardUseCaseInterface.Input
 
   beforeAll(() => {
-    sut = new SaveCreditCardUseCase(uuidGenerator)
+    MockDate.set(new Date())
+
+    sut = new SaveCreditCardUseCase(uuidGenerator, repository)
 
     input = {
       brand: 'anyBrand',
@@ -18,11 +24,33 @@ describe('SaveCreditCardUseCase', () => {
       monthExpiration: '12',
       yearExpiration: '2023'
     }
+
+    uuidGenerator.generate.mockReturnValue('anyUUID')
+    identifierGeneratorMock.mockReturnValue('anyIdentifier')
+  })
+
+  afterAll(() => {
+    MockDate.reset()
   })
 
   test('should call uuidGenerator once', async () => {
     await sut.execute(input)
 
     expect(uuidGenerator.generate).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call CreditCardRepository.save once and with correct values', async () => {
+    await sut.execute(input)
+
+    expect(repository.save).toHaveBeenCalledTimes(1)
+    expect(repository.save).toHaveBeenCalledWith({
+      id: 'anyUUID',
+      identifier: 'anyIdentifier',
+      brand: 'anyBrand',
+      number: '123456XXXXXX1365',
+      expiration: '2023-12',
+      createdAt: new Date()
+
+    })
   })
 })
--- src/application/usecases/credit-card/save-credit-card.usecase.ts
+++ src/application/usecases/credit-card/save-credit-card.usecase.ts
@@ -1,12 +1,36 @@
+import { SaveCreditCardRepositoryInterface } from '@/application/contratcs/credit-card-repository.interface'
 import { SaveCreditCardUseCaseInterface } from '@/application/contratcs/save-credit-card-usecase.interface'
 import { UUIDGeneratorInterface } from '@/application/contratcs/uuid-generator.interface'
 
 export class SaveCreditCardUseCase implements SaveCreditCardUseCaseInterface {
   constructor (
-    private readonly uuidGenerator: UUIDGeneratorInterface
+    private readonly uuidGenerator: UUIDGeneratorInterface,
+    private readonly repository: SaveCreditCardRepositoryInterface
   ) {}
 
   async execute (input: SaveCreditCardUseCaseInterface.Input): Promise<void> {
-    this.uuidGenerator.generate()
+    const number = input.number
+
+    await this.repository.save({
+      id: this.uuidGenerator.generate(),
+      identifier: this.identifierGenerator(),
+      brand: input.brand,
+      number: `${number.substring(0, 6)}XXXXXX${number.slice(number.length - 4)}`,
+      expiration: `${input.yearExpiration}-${input.monthExpiration}`,
+      createdAt: new Date()
+    })
+  }
+
+  private identifierGenerator (): string {
+    const max: number = 5
+    const characters: string = 'ABCDEFGHIJKLMNOPQRSTUVXWYZ0123456789'
+    let str: string = ''
+
+    for (let i = 0; i <= max; i++) {
+      str += characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+
+    const timeStamp = new Date().getTime()
+    return `${str}-${timeStamp}`
   }
 }
