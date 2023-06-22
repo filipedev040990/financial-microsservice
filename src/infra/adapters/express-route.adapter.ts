6c85571b5789a61892441e50c75c3527bd1847f5
Filipe Siqueira <filipedev040990@gmail.com>
2023-06-22 16:15:39 -0300

    feat: add express route adapter

---
+++ src/infra/adapters/express-route.adapter.ts
@@ -0,0 +1,16 @@
+import { ControllerInterface } from '@/application/contratcs/controller.interface'
+import { HttpRequest, HttpResponse } from '@/shared/types'
+import { Request, Response } from 'express'
+
+export const expressRouteAdapter = (controller: ControllerInterface) => {
+  return async (req: Request, res: Response) => {
+    const input: HttpRequest = {
+      body: req.body
+    }
+
+    const { statusCode, body }: HttpResponse = await controller.execute(input)
+    const bodyOutput = (statusCode > 199 && statusCode < 500) ? body : { error: body.error.message }
+
+    res.status(statusCode).json(bodyOutput)
+  }
+}
